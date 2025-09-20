6a475f1edeb5cbf5aa49e694a227325f7f06d8e8
Vu Hoai Nam <156559580+Norl-Wu@users.noreply.github.com>
2025-09-20 09:12:32 +0700

    LoginPages

--- app/src/main/java/vn/edu/usth/crabfood/LoginActivity.java
+++ app/src/main/java/vn/edu/usth/crabfood/LoginActivity.java
@@ -1,8 +1,6 @@
 package vn.edu.usth.crabfood;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,22 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void Login(View view)
-    {
-        startActivity(new Intent(WelcomeActivity.this,LoginActivity.class));
     }
 }
